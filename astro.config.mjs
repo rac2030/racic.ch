4380d1d3e817e973e1ce9fbb334984a56d196850
rac2030 <rac2030@gmail.com>
2026-09-03 21:13:21 +0000

    build: update server configuration in astro.config.mjs to work inside DevContainer accessible from host when serving

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,10 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 4321,
+  },
   site: 'https://racic.ch',
   integrations: [sitemap()],
   markdown: {
